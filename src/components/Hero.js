b130fedf147b5694b564050e6ca7b513b031090f
Mohammad Ashar <itsashar256@gmail.com>
2025-08-16 22:01:12 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   const [text, setText] = useState('');
   const [isDeleting, setIsDeleting] = useState(false);
   const [loopNum, setLoopNum] = useState(0);
-  const [typingSpeed, setTypingSpeed] = useState(300);
+  const [typingSpeed, setTypingSpeed] = useState(150);
 
   useEffect(() => {
     const words = ['Front End Developer', 'Open to Opportunities', 'Code Enthusiast'];
@@ -234,5 +234,6 @@ const Hero = () => {
 };
 
 export default Hero;
+
 
 
